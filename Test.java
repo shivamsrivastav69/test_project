22b80fb7c21ee743bb27c6891f250e4fd668129c
shivamsrivastav69 <shivam144kumar@gmail.com>
2021-09-20 20:37:57 +0530

    my first

---
+++ Test.java
@@ -0,0 +1,14 @@
+import java.util.Date;
+class Test 
+ {
+   public static void main(String[] args) 
+   {
+        String city=new String("bhopal");
+        String Buffer sb=new StringBuffer("i love ");
+        Date today=new Date();
+        System.out.println(city);
+        System.out.println(sb);
+        System.out.println(today);
+    }
+    
+}
